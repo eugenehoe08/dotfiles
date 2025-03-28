063e3d003138e2f875b7c03d91fd75e14db60a75
Eugene <eugenehoe08@hotmail.com>
2025-03-28 19:20:48 +0800

    initial commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", -- disable the title bar but enable the resizable border
+}
+
+return config
---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# System
+
+alias c='clear'
+alias e='exit'
+
+# General
+alias tmp='$HOME/tmp'
+alias ls='eza --all --icons=always'
+alias ll='ls'
+alias zshconfig='nvim ~/.zshrc'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# neovim
+alias v='nvim'
+
